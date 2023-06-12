aa8fd3ce146a1a9cb4a7588ef73056047429528b
Rostyslav Tyshko <tyshko1@gmail.com>
2023-06-12 16:20:27 +0000

    unused vars

--- contract/src/bridge_pool_contract.rs
+++ contract/src/bridge_pool_contract.rs
@@ -203,13 +203,12 @@ pub trait BridgePoolContract<Storage: ContractStorage>: ContractContext<Storage>
 
         let hash =
             &hex::decode(message_hash.clone()).map_err(|_| Error::MessageHashHexDecodingFail)?[..];
-        let sig = &signature_rec;
 
         let s = Secp256k1::new();
         let msg = Message::from_slice(hash).unwrap();
-        let mut sig_compact: Vec<u8> = sig.r().to_bytes().to_vec();
-        sig_compact.extend(&sig.s().to_bytes().to_vec());
-        let id_u8: u8 = From::from(sig.recovery_id());
+        let mut sig_compact: Vec<u8> = signature_rec.r().to_bytes().to_vec();
+        sig_compact.extend(&signature_rec.s().to_bytes().to_vec());
+        let id_u8: u8 = From::from(signature_rec.recovery_id());
         let sig_v = secp256k1::ecdsa::RecoveryId::from_i32(id_u8 as i32).unwrap();
         let rec_sig =
             secp256k1::ecdsa::RecoverableSignature::from_compact(&sig_compact, sig_v).unwrap();
@@ -238,13 +237,12 @@ pub trait BridgePoolContract<Storage: ContractStorage>: ContractContext<Storage>
             return Err(Error::InvalidSigner);
         }
 
-        let client_addr = actor;
         runtime::call_versioned_contract::<()>(
             token,
             None,
             "transfer",
             runtime_args! {
-                "recipient" => client_addr,
+                "recipient" => actor,
                 AMOUNT => amount
             },
         );
