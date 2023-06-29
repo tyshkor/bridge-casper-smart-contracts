e5af583b2d0d363a67c8c1a9ba8a83a28d6c23db
Rostyslav Tyshko <tyshko1@gmail.com>
2023-06-29 12:51:51 +0000

    rm dupl

--- contract/src/bridge_pool_contract.rs
+++ contract/src/bridge_pool_contract.rs
@@ -161,10 +161,7 @@ pub trait BridgePoolContract<Storage: ContractStorage>: ContractContext<Storage>
         let actor = detail::get_immediate_caller_address()
             .unwrap_or_revert_with(Error::ImmediateCallerFail);
 
-        let client_address = detail::get_immediate_caller_address()
-            .unwrap_or_revert_with(Error::ImmediateCallerFail);
-
-        let client_address_string: String = client_address.try_into()?;
+        let client_address_string: String = actor.try_into()?;
 
         if caller != client_address_string {
             return Err(Error::WrongCaller);
@@ -243,13 +240,12 @@ pub trait BridgePoolContract<Storage: ContractStorage>: ContractContext<Storage>
             return Err(Error::InvalidSigner);
         }
 
-        let client_addr = actor;
         runtime::call_versioned_contract::<()>(
             token,
             None,
             ERC20_ENTRY_POINT_TRANSFER,
             runtime_args! {
-                RECIPIENT => client_addr,
+                RECIPIENT => actor,
                 AMOUNT => amount
             },
         );
